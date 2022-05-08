eb7459e871054f3ec91ccac748d25e487fd5e03e
vqstark <vuquan12112001@gmail.com>
2022-05-08 11:09:02 +0700

    Add create product

--- src/main/java/com/example/th2musicdownload/controller/ProductController.java
+++ src/main/java/com/example/th2musicdownload/controller/ProductController.java
@@ -29,6 +29,44 @@ public class ProductController {
         return "viewProducts";
     }
 
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("product", new Product());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@RequestParam String productCode,
+                                @RequestParam String description,
+                                @RequestParam String price,
+                                Model model){
+        String message = "";
+        Double price1 = 0d;
+        try{
+            price1 = Double.parseDouble(price);
+            if(price1 < 0){
+                message = "You must enter product code for product";
+            }
+        }catch(NumberFormatException e){
+            price1 = 0d;
+        }
+        Product product = new Product(productCode, description, price1);
+        model.addAttribute("product", product);
+        if(productCode==null || productCode.length()==0){
+            message = "You must enter product code for product";
+            model.addAttribute("message", message);
+            return "create";
+        }
+        if(description==null || description.length()==0){
+            message = "You must enter description for product";
+            model.addAttribute("message", message);
+            return "create";
+        }
+
+        productRepository.save(new Product(productCode, description, Double.parseDouble(price)));
+        return "redirect:/product/displayProducts";
+    }
+
     @GetMapping("/delete/{productCode}")
     public String delete(@PathVariable String productCode, Model model){
         Product product = productRepository.findByProductCode(productCode);
